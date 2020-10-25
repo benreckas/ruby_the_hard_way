e90a24331ae2fc3499b03fe54aea708e1d9bafa8
Ben Reckas <benreckas@agrian.com>
2020-10-25 09:11:52 -0700

    ex22/23

---
+++ ex22.rb
@@ -0,0 +1,3 @@
+# Exercise to memorize all the symbols
+# I'm not doing this. I think the best way to learn is by doing,
+# not making memorization tables.
---
+++ ex23.rb
@@ -0,0 +1,3 @@
+# Exercise to read random Ruby code on Github
+# Also not doing this. I've been exposed to reading Ruby
+# through PRs for projects at work.
