ba4b2bcc4210ee1296a76df16436638a3587df2d
Ben Reckas <benreckas@agrian.com>
2020-11-03 23:16:03 -0800

    ex34

---
+++ exercises/ex34.rb
@@ -0,0 +1,25 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# Animal at 1
+puts animals[1]
+
+# The third animal
+puts animals[2]
+
+# The first animal
+puts animals[0]
+
+# Animal at 3
+puts animals[3]
+
+# The fifth animal
+puts animals[4]
+
+# Animal at 2
+puts animals[2]
+
+# The sixth animal
+puts animals[5]
+
+# Animal at 4
+puts animals[4]
